ee18a75947078fc1cfccb1b9e1ec783abca036f1
Dovydas Valančius <dovydas.valancius@gmail.com>
2024-06-04 14:19:48 +0300

    Added columns, but they still need some adjustment

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import pygame
 # constant variables
 WIDTH = 1280
 HEIGHT = 720
-LANE_START = 280
-LANE_HEIGHT = HEIGHT/6 - 10
+LANE_START_X = 280
+LANE_START_Y = HEIGHT/6
+LANE_HEIGHT = HEIGHT/6
+COL_START = LANE_START_X - 2
+COL_WIDTH = (WIDTH - LANE_START_X - 10) / 8
 
 # pygame setup
 pygame.init()
@@ -14,21 +17,31 @@ running = True
 pygame.display.set_caption('Spellbound Amnesia')
 
 # variables
-border_thickness = 5
-lane_color = (0, 0, 0) # black
+
 selected_lane_color = (255, 0 ,0) # red
+border_thickness = 5
 selected_lane = None # stores info on which lane is selected
+border_thickness = 5
+margin = 10
 
 # objects
 # lanes
 lanes = [
-    pygame.Rect(LANE_START, HEIGHT/6*3, WIDTH - LANE_START - 10, LANE_HEIGHT),
-    pygame.Rect(LANE_START, HEIGHT/6*4, WIDTH - LANE_START - 10, LANE_HEIGHT),
-    pygame.Rect(LANE_START, HEIGHT/6*5, WIDTH - LANE_START - 10, LANE_HEIGHT)
+    pygame.Rect(LANE_START_X, LANE_START_Y*3 - margin, WIDTH - LANE_START_X - margin, LANE_HEIGHT),
+    pygame.Rect(LANE_START_X, LANE_START_Y*4 - margin, WIDTH - LANE_START_X - margin, LANE_HEIGHT),
+    pygame.Rect(LANE_START_X, LANE_START_Y*5 - margin, WIDTH - LANE_START_X - margin, LANE_HEIGHT)
 ]
 
-# enemies
+# columns
+def add_columns(number_of_cols):
+    spots = []
+    for spot in range(number_of_cols):
+        col = pygame.Rect(COL_START + (COL_WIDTH * spot + 1), LANE_START_Y * 3 - margin, COL_WIDTH, HEIGHT - LANE_START_Y - margin)
+        spots.append(col)
 
+    return spots
+
+# enemies
 
 while running:
     # poll for events
@@ -50,12 +63,16 @@ while running:
     screen.fill((102, 140, 255))
 
     # drawing lanes
-
     for i, lane in enumerate(lanes):
         if i == selected_lane and selected_lane is not None:
             pygame.draw.rect(screen, selected_lane_color, lane, border_thickness)
         else:
-            pygame.draw.rect(screen, lane_color, lane, border_thickness)
+            pygame.draw.rect(screen, "black", lane, border_thickness)
+
+    # draw columns/spots
+    spots = add_columns(8)
+    for col in spots:
+        pygame.draw.rect(screen, "black", col, border_thickness)
 
 
     # rendering game here
