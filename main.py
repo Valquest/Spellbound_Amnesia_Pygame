667b969308c7720178738103b139acbbc649d68d
Dovydas Valančius <dovydas.valancius@gmail.com>
2024-06-04 12:29:50 +0300

    Initial code setup
    Removed all the "dabble" and are now preparing to build the base of the game combat mechanics

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280,720))
+clock = pygame.time.Clock()
+running = True
+pygame.display.set_caption('Spellbound Amnesia')
+
+
+while running:
+    # poll for events
+    # pygame.Quit event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
+
+
+    # rendering game here
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
